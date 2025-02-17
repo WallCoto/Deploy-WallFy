19827c1e8f34629c9b9acbf0e744517708073934
Wallace <wallacecicero3280@gmail.com>
2025-02-17 20:36:57 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -2,22 +2,29 @@ import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
 
+const __dirname = path.resolve();
+
 const app = express();
 const PORT = 3005;
 
 app.use(cors());
 // app.use(cors({ origin: "http://localhost:5173" }));
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
+app.use(express.static(path.join(__dirname, "../front-end/dis")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dis/index.html"));
+});
 app.listen(PORT, () => {});
